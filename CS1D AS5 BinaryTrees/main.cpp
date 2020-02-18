ee9a47397e82bc75847a66e445f48bf23a2ae47b
Nick Reardon <32754140+ThingOfNightmare@users.noreply.github.com>
2020-02-17 19:42:16 -0800

    Add project files.

---
+++ CS1D AS5 BinaryTrees/main.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * AUTHOR 	  		: Nick Reardon
+ * Assignment #5	: Binary Trees
+ * CLASS			: CS1D
+ * SECTION			: MW - 2:30p
+ * DUE DATE			: 02 / 19 / 20
+ *****************************************************************************/
+#include "main.h"
+
+using std::cout; using std::endl;
+
+
+int main()
+{
+
+	/*
+	 * HEADER OUTPUT
+	 */
+	PrintHeader(cout, "Prompt.txt");
+
+	/********************************************************************/
+
+
+
+	system("pause");
+	return 0;
+}
